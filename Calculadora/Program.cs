4199b677aa65320c52b005662c410f4a4827709a
wxnshw <wender.ribeiro8@gmail.com>
2023-11-13 01:40:01 -0300

    first commit

---
+++ Calculadora/Program.cs
@@ -0,0 +1,12 @@
+﻿using System.
+
+namespace Program
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            CalculadoraImp
+        }
+    }
+}
---
+++ Calculadora/Services/CalculadoraImp.cs
@@ -0,0 +1,12 @@
+﻿namespace Calculadora.Services
+{
+    public class CalculadoraImp
+    {
+        public int Somar(int num1, int num2)
+        {
+            return num1 + num2;
+        }
+    }
+}
+
+
